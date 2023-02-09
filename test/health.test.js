6210f54297cead33edaf3c43be9685e44b4367d6
Evan Sosenko <razorx@evansosenko.com>
2023-02-09 00:09:13 -0800

    --wip-- [skip ci]

--- test/health.test.js
+++ test/health.test.js
@@ -4,7 +4,6 @@ import { getTestServer } from './fixtures/server.js'
 
 test('GET: /health', async (t) => {
   const server = await getTestServer(t)
-  server.run()
   const res = await fetch(`${server.origin}/health`)
   const data = await res.json()
   t.snapshot(data, 'data')
--- test/root.test.js
+++ test/root.test.js
@@ -4,7 +4,6 @@ import { getTestServer } from './fixtures/server.js'
 
 test('GET: /', async (t) => {
   const server = await getTestServer()
-  server.run()
   const res = await fetch(`${server.origin}/`)
   const data = await res.json()
   t.snapshot(data, 'data')
