548b247999013c1037765fb6fb9b4342aca7ea22
Evan Sosenko <razorx@evansosenko.com>
2023-02-08 22:34:46 -0800

    WIP JS

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-import { boot } from './server/index.js'
+import process from 'node:process'
 
-boot()
+import { createLogger, createServer, getConfig } from './index.js'
+
+try {
+  const config = await getConfig(process.env)
+  const logger = createLogger(config)
+  const server = createServer({ ...config, logger })
+  await server.run()
+  logger.info(server.origin)
+} catch (err) {
+  const logger = createLogger()
+  logger.error({ err })
+  process.exit(1)
+}
