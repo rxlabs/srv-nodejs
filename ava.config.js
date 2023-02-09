2a709c924ca1594ff63ae4651ca66ba0be770969
Evan Sosenko <razorx@evansosenko.com>
2023-02-08 21:15:06 -0800

    Update ava.config.js

--- ava.config.js
+++ ava.config.js
@@ -1,3 +1,11 @@
-export default {
-  files: ['**/*.spec.js', '!package/**/*']
+import process from 'node:process'
+
+export default () => {
+  const ext = isTrue(process.env.SYSTEM_TEST) ? 'test' : 'spec'
+  return {
+    ignoredByWatcher: ['tmp/**/*'],
+    files: [`**/*.${ext}.js`, '!package/**/*']
+  }
 }
+
+const isTrue = (value) => ['true', '1'].includes(value?.toLowerCase())
