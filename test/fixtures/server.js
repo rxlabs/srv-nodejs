6d3b1e1b6b6f049745983ef9492367d41f467e70
Evan Sosenko <razorx@evansosenko.com>
2023-02-09 21:18:48 -0800

    --wip-- [skip ci]

--- test/fixtures/server.js
+++ test/fixtures/server.js
@@ -5,7 +5,7 @@ import getPort from '@ava/get-port'
 import { createLogger, createServer, getConfig } from '../../index.js'
 
 export const getTestServer = async (t) => {
-  const PORT = env.PORT ?? (await getPort())
+  const PORT = await getPort()
   const config = await getConfig({ PORT, ...env })
   const logger = createLogger({ config, t })
   const server = createServer({ config, logger })
